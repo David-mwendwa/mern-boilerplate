a441ff0d49ef7d6540710a087afce24d4b7dc2a9
techdave <klatzdavie19@gmail.com>
2023-02-02 17:42:12 +0300

    client: add FormRow component util and some globale styles FormRow component receives some values and returns and form label + input

---
+++ client/src/components/FormRow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+/**
+ * @param {*}, type, name, value, handleChange, labelText
+ * @returns html element containing label and the input
+ */
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
+  return (
+    <div className='form-row'>
+      <label htmlFor={name} className='form-label'>
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className='form-input'
+      />
+    </div>
+  );
+};
+
+export default FormRow;
